c1b0a7c11ae0d56857adbe2672a12b1ca6a36bd3
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-02-16 00:10:59 -0600

    Use builder config (#1)
    * Prototype injection script
    
    * Prototype build generation
    
    * Allow configurable destination path
    
    * Prototype build
    
    * Update README
    
    * Update Cargo

---
+++ example-app/src/main.rs
@@ -0,0 +1,15 @@
+mod cargo_pkg_info;
+use cargo_pkg_info::CargoPkgInfo;
+
+fn main() {
+    let cargo_pkg_info = CargoPkgInfo::new();
+
+    println!(
+        "{:?} {:?} {:?} {:?} {:?}",
+        cargo_pkg_info.app_name(),
+        cargo_pkg_info.crate_name(),
+        cargo_pkg_info.app_version(),
+        cargo_pkg_info.build_target(),
+        cargo_pkg_info.build_time_utc(),
+    );
+}
