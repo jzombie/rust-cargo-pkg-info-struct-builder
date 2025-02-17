21cd0492e09c00a5c01bfbcaad98d8359a7bab44
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-02-17 17:43:35 -0600

    Expose `set_multi_line_cargo_env_var`

--- cargo-pkg-info-struct-builder/src/lib.rs
+++ cargo-pkg-info-struct-builder/src/lib.rs
@@ -1,2 +1,4 @@
 pub mod inject_build_metadata;
-pub use inject_build_metadata::{inject_build_metadata, set_cargo_env_var};
+pub use inject_build_metadata::{
+    inject_build_metadata, set_cargo_env_var, set_multi_line_cargo_env_var,
+};
